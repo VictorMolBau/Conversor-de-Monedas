f570d317ca6949b7f88d0f94208704bfea6e9563
VictorMolBau <116696703+VictorMolBau@users.noreply.github.com>
2024-06-13 20:37:24 -0600

    Add files via upload

---
+++ ConversorDeMonedas/src/Menu.java
@@ -0,0 +1,15 @@
+public class Menu {
+    public static void mensajeInicial() {
+        System.out.println("************************");
+        System.out.println("* CONVERSOR DE MONEDAS *");
+        System.out.println("************************");
+        System.out.println("""
+                1.- Peso Colombiano
+                2.- Peso Argentino
+                3.- Peso Mexicano
+                4.- Real Brasileño
+                5.- Dolar Americano
+                6.- Dolar Canadiense
+                7.- Euro""");
+    }
+}
---
+++ ConversorDeMonedas/src/Reiniciar.java
@@ -0,0 +1,5 @@
+public class Reiniciar {
+    public static void reinicio() {
+        System.out.println("Deseas realizar otra conversión?");
+    }
+}
